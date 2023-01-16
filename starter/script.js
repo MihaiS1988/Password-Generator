ac974a2f387a3d92fa9546b96b0bf23b2b467053
MihaiS1988 <117821906+MihaiS1988@users.noreply.github.com>
2023-01-16 16:41:59 +0000

    Added the function to prompt user of password option

--- starter/script.js
+++ starter/script.js
@@ -94,10 +94,24 @@ let loCharacters ;        // This variable is storing the user input  choice as 
 let upCharacters ;        // This variable is storing the user input  choice as true or false for the upperCase  characters. 
 let numCharacters ;       // This variable is storing the user input  choice as true or false for the numerical  characters. 
 
-// Function to prompt user for password options
+// Below is the Function to prompt user the password options
 function getPasswordOptions() {
-
-}
+  const regex = /^(1[0-9]|[2-5][0-9]|6[0-4])$/; ///This regular expression is matching all the numbers between 10 an 64 included, credit to 3widgets.com (I have used their regex numeric generator)
+        passLength = prompt("Please choose a number between 10 and 64");
+        while (passLength !== NaN && regex.test(passLength) !== true) {   
+          if (passLength === null) {  //This 'if' statement makes sure that if you press cancel then breaks the loop 
+            break;
+          }
+          passLength = prompt("Oopps! Wrong input, Please choose a number between 10 and 64");
+        }
+        console.log(passLength);
+        if (passLength !== null){    // If the user will press cancel instead of choosing a number then the function of choosing password options will not execute
+            spCharacters = confirm("If you want to include special characters press 'OK' if not, press Cancel");
+            upCharacters = confirm("If you want to include upperCase characters press 'OK' if not, press 'Cancel'");
+            numCharacters = confirm("If you want to include numerical characters press 'OK' if not, press 'Cancel'");
+            loCharacters = confirm("If you want to include lowerCase characters press 'OK' if not, press 'Cancel'");
+        }
+};
 
 // Function for getting a random element from an array
 function getRandom(arr) {
